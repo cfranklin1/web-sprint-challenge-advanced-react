03a5be336492b58c68781227acafb5e4ae6da08d
camille <spacejamilli20@gmail.com>
2021-03-12 12:03:34 -0600

    fetching plant data/useForm created

--- src/hooks/useForm.js
+++ src/hooks/useForm.js
@@ -1,1 +1,22 @@
+import {useState} from 'react';
+import useLocalStorage from './useLocalStorage';
+
 // write your custom hook here to control your checkout form
+const useForm = (initialValues) => {
+    const [showSuccessMessage, setShowSuccessMessage] = useState(false);
+    const [values, setValues] = useLocalStorage("form", initialValues); 
+
+
+    const handleChanges = (e) => {
+        setValues({ ...values, [e.target.name]: e.target.value });
+      };
+    
+      const handleSubmit = (e) => {
+        e.preventDefault();
+        setShowSuccessMessage(true);
+      };
+
+      return [handleChanges, handleSubmit, showSuccessMessage, values];
+}
+
+export default useForm;
